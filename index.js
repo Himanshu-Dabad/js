70f1fa77521c0fb66b5c316b6f5c9ca1e06c8ae6
Himanshu-Dabad <jnvhimanshuyadav@gmail.com>
2024-06-08 11:55:45 +0530

    js

---
+++ index.js
@@ -0,0 +1,77 @@
+// //var
+// var num = 10
+// {
+//     num = 20
+// }
+// console.log("var",num)
+// //let
+// let num1 = 10
+// {
+// num1 = 20
+// } 
+// console.log("name","raj")
+// //const
+// const num2 = 10
+// {
+//     num2.name = "Hello"
+// }
+// console.log("const",num2)
+
+// var num = 10
+// {
+//     var num = 20
+//     num = 30
+//     console.log(num)
+// }
+// console.log(num)
+// let num1 = 10
+// {
+//     let num1 = 20
+//     num1 = 30
+//     console.log(num1)
+// }
+// console.log(num1)
+
+// const num2 = 10
+// {
+//     const num2 = 20
+    
+//     console.log(num2)
+// }
+// console.log(num2)
+
+// let num1 = 10;
+// let num2 = 5;
+
+// let sum = num1 + num2;
+// let sub = num1 - num2;
+// let mul = num1 * num2;
+// let div = num1 / num2;
+// let mod = num1 % num2;
+
+// console.log(sum,sub,mul,div,mod);
+
+// let num1 = 5
+// let num2 = 10
+// let sum = --num1 + ++num2 + num1-- + num2++
+// console.log(num1,num2,sum)
+
+//let a = 20
+// let b = 35
+// let c = 30
+//let result = (a >= 18)? "Eligible for vote":"Not eligible for vote"
+
+// let obj = {}
+// let arr
+// console.log(typeof checkin)
+
+//  console.log(typeof checkout)
+// console.log(typeof obj)
+// console.log(typeof arr)
+
+//console.log(result)
+let a=10;
+let b="";
+
+let result=(a && b)?"yes":"no";
+console.log(result);
